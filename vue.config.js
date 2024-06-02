2925af3a10cc266365f1011cd68653d2e6aed976
Robert <14954796+RobertLii@users.noreply.github.com>
2024-06-02 12:55:05 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/UnifiedAuthPlatformPortal/" : "/",
+  // publicPath: process.env.NODE_ENV === "production" ? "/UnifiedAuthPlatformPortal/" : "/",
   configureWebpack: {
     devtool: 'source-map'
   }
